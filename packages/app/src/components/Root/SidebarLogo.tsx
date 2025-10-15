5ce9b7d517246fce3ef6a40fce2ad5f2f0e08890
Andre Fernandes <andre@vertigo.com.br>
2025-10-15 08:34:28 -0300

    upgraded global-header, fix sidebar logo

--- packages/app/src/components/Root/SidebarLogo.tsx
+++ packages/app/src/components/Root/SidebarLogo.tsx
@@ -21,6 +21,7 @@ import { Link, useSidebarOpenState } from '@backstage/core-components';
 import { configApiRef, useApi } from '@backstage/core-plugin-api';
 
 import { makeStyles } from 'tss-react/mui';
+import { useTheme } from '@mui/material/styles';
 
 import LogoFull from './LogoFull';
 import LogoIcon from './LogoIcon';
@@ -52,13 +53,49 @@ const LogoRender = ({
   );
 };
 
+type LogoURLs =
+  | {
+      /** The logo that will be used in global headers with a light-coloured background */
+      light: string;
+      /** The logo that will be used in global headers with a dark-coloured background */
+      dark: string;
+    }
+  | string
+  | undefined;
+
+// same logic from global header
+const useFullLogo = (): string | undefined => {
+  const theme = useTheme();
+  const mode = theme.palette.mode;
+  const isDarkMode = theme.palette.mode === 'dark';
+  const configApi = useApi(configApiRef);
+
+  /** The fullLogo config specified by app.branding.fullLogo */
+  const fullLogo = configApi.getOptional<LogoURLs>('app.branding.fullLogo');
+  
+  /** The dark theme logo config specified by app.branding.fullLogoDark */
+  const fullLogoDark = configApi.getOptional<string>('app.branding.fullLogoDark');
+
+  /** The URI of the logo specified by app.branding.fullLogo */
+  const fullLogoURI =
+    typeof fullLogo === 'string'
+      ? fullLogo
+      : fullLogo?.[mode];
+  
+  /** Use fullLogoDark for dark theme if available, otherwise fall back to fullLogoURI */
+  const configLogoURI = isDarkMode && fullLogoDark ? fullLogoDark : fullLogoURI;
+
+  return configLogoURI ?? undefined;
+};
+
 export const SidebarLogo = () => {
   const { classes } = useStyles();
   const { isOpen } = useSidebarOpenState();
   const configApi = useApi(configApiRef);
-  const logoFullBase64URI = configApi.getOptionalString(
-    'app.branding.fullLogo',
-  );
+  const logoFullBase64URI = useFullLogo()
+  // const logoFullBase64URI = configApi.getOptionalString(
+  //   'app.branding.fullLogo',
+  // );
   const fullLogoWidth = configApi
     .getOptional('app.branding.fullLogoWidth')
     ?.toString();
