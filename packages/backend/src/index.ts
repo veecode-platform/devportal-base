a1888e9f2f70e05216bb67b113ab2f159970026c
Andre Fernandes <andre@vertigo.com.br>
2025-10-16 05:18:47 -0300

    fix backend translation, release 1.1.10

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -201,7 +201,7 @@ backend.add(import('@backstage/plugin-signals-backend'));
 
 backend.add(import('@internal/plugin-dynamic-plugins-info-backend'));
 backend.add(import('@internal/plugin-scalprum-backend'));
-backend.add(import('@internal/plugin-translation-backend'));
+backend.add(import('@red-hat-developer-hub/backstage-plugin-translations-backend'));
 
 backend.add(userSettingsBackend);
 
